03353c8259176abada11870873fb3b165628cf1a
KarolSoaresd <85362917+KarolSoaresd@users.noreply.github.com>
2023-10-31 17:32:13 -0300

    testes analise

---
+++ cypress/integration/testes-avancados/testes-integracao.spec.js
@@ -0,0 +1,9 @@
+describe('Validar site Mentorama', () => {
+  beforeEach(() => {
+    cy.visit('https://mentorama.com.br/')
+  })
+
+  it('Devo receber negativa ao acessar com dados náo cadastrados', () => {
+
+  })
+})
